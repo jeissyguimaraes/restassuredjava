48ca81d0266abd5f3a3e5b7a0f2133fc85eb6f09
Jeissy <jeissyguimaraes@gmail.com>
2021-09-28 16:13:18 -0300

    alterações globalStaticParameters

--- src/test/java/com/apimantisbugtracker/defaultParameters/GlobalStaticParameters.java
+++ src/test/java/com/apimantisbugtracker/defaultParameters/GlobalStaticParameters.java
@@ -7,11 +7,6 @@ public class GlobalStaticParameters {
     //BuscaDadosUsuario
     public static String senhaUsuario = "abc*123";
 
-    //BuscaDadosFiltro
-    public static String name ="FILTRO";
-    public static CharSequence idUsuario ="1";
-    public static CharSequence idProjeto ="1";
-    public static String filtroString ="filtro 1";
 
 
 
